4ff7de0ffd84bfd673d2836522ef6a7ad9d63392
AmamiSQ <r_zimmerman@outlook.com>
2022-10-12 14:35:04 +0200

    images

--- src/sketch.js
+++ src/sketch.js
@@ -1,11 +1,14 @@
 var canvas; 
-let bg;
+let bg, pipe, bird;
 
 function setup() {
   canvas = createCanvas(600, 1000);
   canvas.position(260, 200);
 
-  bg = loadImage('images/background.png');
+  bg = loadImage('images/background.jpg');
+  pipe = loadImage('images/pipes.png');
+  bird = loadImage('images/bird.png');
+
 }
 
 function draw() {
